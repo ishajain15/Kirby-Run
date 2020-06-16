9d426c0d8dc19079a1e181b97d727c037023eab2
Isha Jain <write2ishajain@gmail.com>
2020-06-16 15:59:01 -0400

    Uploaded the game files. Will need to update the tile maps later on.

---
+++ Platformer/src/Main/Game.java
@@ -0,0 +1,15 @@
+package Main;
+
+import javax.swing.JFrame;
+
+public class Game {
+	public static void main(String[] args) {
+		JFrame window = new JFrame("Game");
+		window.setContentPane(new GamePanel());
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.pack();
+		window.setVisible(true);
+	}
+
+}
