f1c14e3625442c0d5bbd101bfcf273607700e1ac
Isha Jain <write2ishajain@gmail.com>
2020-06-17 21:23:59 -0400

    Updated tileset

--- Platformer/src/GameState/Level1State.java
+++ Platformer/src/GameState/Level1State.java
@@ -22,7 +22,7 @@ public class Level1State extends GameState {
 
 	public void init() {
 		tileMap = new TileMap(30);
-		tileMap.loadTiles("/Tilesets/grasstileset.gif");
+		tileMap.loadTiles("/Tilesets/level1.png");
 		tileMap.loadMap("/Maps/level1-1.map");
 		tileMap.setPosition(0, 0);
 		tileMap.setTween(1);
