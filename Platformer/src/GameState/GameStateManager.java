5e2fa34110911f8471c65322c8aa1570a82ceea0
ishajain <write2ishajain@gmail.com>
2020-08-13 22:20:23 -0400

    Changed GameStateManager to use arrays instead of arraylists

--- Platformer/src/GameState/GameStateManager.java
+++ Platformer/src/GameState/GameStateManager.java
@@ -1,41 +1,60 @@
 package GameState;
 
-import java.util.ArrayList;
-
 public class GameStateManager {
 
-	private ArrayList<GameState> gameStates;
+	private GameState[] gameStates;
 	private int currentState;
-	
+
+	public static final int NUMGAMESTATES = 2;
 	public static final int MENUSTATE = 0;
 	public static final int LEVEL1STATE = 1;
-	
+
 	public GameStateManager() {
-		gameStates = new ArrayList<GameState>();
-		
+		gameStates = new GameState[NUMGAMESTATES];
+
 		currentState = MENUSTATE;
-		gameStates.add(new MenuState(this));
-		gameStates.add(new Level1State(this));
+		loadState(currentState);
 	}
-	
+
+	private void loadState(int state) {
+		if (state == MENUSTATE)
+			gameStates[state] = new MenuState(this);
+
+		if (state == LEVEL1STATE)
+			gameStates[state] = new Level1State(this);
+	}
+
+	private void unloadState(int state) {
+		gameStates[state] = null;
+	}
+
 	public void setState(int state) {
+		unloadState(currentState);
 		currentState = state;
-		gameStates.get(currentState).init();
+		loadState(currentState);
+		currentState = state;
+		// gameStates[currentState].init();
 	}
-	
+
 	public void update() {
-		gameStates.get(currentState).update();
+		try {
+			gameStates[currentState].update();
+		} catch (Exception e) {
+		}
 	}
-	
+
 	public void draw(java.awt.Graphics2D g) {
-		gameStates.get(currentState).draw(g);
+		try {
+			gameStates[currentState].draw(g);
+		} catch (Exception e) {
+		}
 	}
-	
+
 	public void keyPressed(int k) {
-		gameStates.get(currentState).keyPressed(k);;
+		gameStates[currentState].keyPressed(k);
 	}
-	
+
 	public void keyReleased(int k) {
-		gameStates.get(currentState).keyReleased(k);
+		gameStates[currentState].keyReleased(k);
 	}
 }
