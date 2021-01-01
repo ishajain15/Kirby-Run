bc8888563b4807fb79b59259a4e134d4f776f293
Isha Jain <ishajainpdx@gmail.com>
2021-01-01 10:57:21 -0500

    done

--- Platformer/src/Entity/FireBall.java
+++ Platformer/src/Entity/FireBall.java
@@ -32,7 +32,7 @@ public class FireBall extends MapObject {
 
 		// load sprites
 		try {
-			BufferedImage spritesheet = ImageIO.read(getClass().getResourceAsStream("/Sprites/Player/fireball.gif"));
+			BufferedImage spritesheet = ImageIO.read(getClass().getResourceAsStream("/Sprites/Player/star.png"));
 
 			sprites = new BufferedImage[4];
 			for (int i = 0; i < sprites.length; i++) {
